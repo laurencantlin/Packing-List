8eb904c7662b593f5b6c6fbe1f79141a3669ef4b
laurencantlin <laurenrose5321@gmail.com>
2018-02-02 12:27:28 -0600

    adds functionality to wireframe html

---
+++ wireframeUI.js
@@ -0,0 +1,18 @@
+
+// ------MY TRIPS HOME ------
+function clickCancel(){
+console.log("click cancel trip");
+
+}
+
+$("#addTripBtn").click(clickAddTrip);
+
+
+// -----ADD TRIP FORM -------
+function clickCreateTrip(){
+    console.log("click add trip");
+    
+    }
+    
+    $("#addTripBtn").click(clickCreateTrip);
+    
