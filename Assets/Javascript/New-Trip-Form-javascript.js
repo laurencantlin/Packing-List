96c2414b192c95ab82ad6b061b44d7fc9ea92294
laurencantlin <laurenrose5321@gmail.com>
2018-02-15 19:16:59 -0600

    adds jsdoc documentation to js files

--- Assets/Javascript/New-Trip-Form-javascript.js
+++ Assets/Javascript/New-Trip-Form-javascript.js
@@ -1,13 +1,12 @@
-//AUTOFILL FUNCTION
+/**
+ * Makes API call to Google autocomplete api for a given destination, and then renders those predictions on the page. Sets Destination input value to prediction on click.
+ */
 var autoFill = function () {
     var locInput = $("#tripLocation");
     locInput.val();
     var apiKey = "AIzaSyD4uplfsc5qPH6Z92HT9JSStvydTmRTyMs";
     var queryURL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=" + apiKey + "&input=" + locInput.val() + "&type=geocode";
-
     var predictionsView = $("#predictionsView");
-
-
     $.ajax({
         url: 'https://cors-anywhere.herokuapp.com/' + queryURL,
         method: "GET",
@@ -18,7 +17,6 @@ var autoFill = function () {
             $("#predictionsView").append("<p> Zero Results </p>");
         
         }
-        
         var renderPredictions = function () {
             var predictionArr = [];
 
@@ -26,72 +24,49 @@ var autoFill = function () {
                 var newPredict = $("<p>")
                 var predictions = [];
                 predictions = response.predictions[i].description;
-                console.log(predictions);
                 var city=predictions.split(",");
                 var city=city.slice(-3, city.length);
                 city =city.join(",")
-                console.log(city);
                 newPredict.text(city);
                 newPredict.addClass("prediction");
                 predictionsView.append(newPredict);
             }
             $(".prediction").click(autofillClick);
             function autofillClick() {
-                console.log("clickssss");
                 locInput.val(this.innerText);
                 predictionsView.hide();
                 $("#tripName").val(this.innerText)
             }
             $(".prediction").hover(hoverOn, hoverOff);
             function hoverOn() {
-                console.log("hoveron")
-
-                console.log(this)
                 $(this).addClass("autofillHover");
             }
             function hoverOff() {
-                console.log("hoveroff")
                 $(this).removeClass("autofillHover");
             }
-
-            // $(locField).blur(autofillBlur);
-            // function autofillBlur(){
-            //     console.log("focusout")
-            //     predictionsView.hide();
-
-            // }
-
         }
-        // renderPredictions();
         if (locInput.val() !== "") {
             predictionsView.show();
             renderPredictions();
-
         }
         else { predictionsView.hide() }
     })
 }
 
-
-$("#tripLocation").keyup(autoFill);
-
-
-
-
-// SUBMIT ADD TRIP FORM FUNCTION
+/**
+ * Adds creates new trip object and pushes that object to Trips Array in local storage
+ * @param {string} event 
+ */
 function addTrip(event) {
     event.preventDefault();
     var tripsArray = [];
     if (localStorage.getItem("Trips Array")) {
         tripsArray = JSON.parse(localStorage.getItem("Trips Array"));
-
     }
-
     $.ajax({
         type: "GET",
         url: 'https://cors-anywhere.herokuapp.com/' + "https://api.openweathermap.org/data/2.5/weather?q=" + $("#tripLocation").val() + "&appid=8a03f969205ca8695bf44e2bd8b84126",
     }).done(function (result) {
-        console.log(result);
         var temp = Math.floor(result.main.temp * 9 / 5 - 459.67);
         var trip = {
             tripName: $("#tripName").val(),
@@ -110,20 +85,15 @@ function addTrip(event) {
                 Toiletries: [],
             }
         }
-
         tripsArray.push(trip);
-        console.log(tripsArray);
         localStorage.setItem("Trips Array", JSON.stringify(tripsArray));
         localStorage.setItem("SetTrip", JSON.stringify(trip));
-
         window.location.href = "Pack-List.html";
-
     });
 }
 
-
+$("#tripLocation").keyup(autoFill);
 $("#create-trip-btn").click(addTrip);
-// $("#weatherInfo").text(SetTrip.weather);
 
 
 
--- Assets/Javascript/Pack-List.js
+++ Assets/Javascript/Pack-List.js
@@ -1,14 +1,13 @@
-// -----------------------------------
-//  RENDER PACKLIST 
+/** 
+ *  renderPackList 
+*/
 function renderPackList() {
     var parsed = [{ packingList: { Default: [] } }];
     if (localStorage.getItem("SetTrip")) {
         parsed = JSON.parse(localStorage.getItem("SetTrip"));
-        console.log(parsed, "comment")
     }
     var SetTrip = parsed
     var cats = Object.keys(SetTrip.packingList);
-    console.log(cats)
     for (var i = 0; i < cats.length; i++) {
         var items = SetTrip.packingList[cats[i]];
         if (items.length !== 0) {
@@ -27,10 +26,8 @@ function renderPackList() {
         newListItem = $(newListItem).append(newLink);
         $(newListItem).insertBefore("li.divider");
     }
-
     var formattedStartDate = moment(SetTrip.startDate, "YYYY-MM-DD").format("ll");
     var formattedEndDate = moment(SetTrip.endDate, "YYYY-MM-DD").format("ll");
-
     $("#trip-name").text(SetTrip.tripName);
     $("#destination").text(SetTrip.destination);
     $("#date").text(formattedStartDate + " - " + formattedEndDate);
@@ -43,31 +40,22 @@ function renderCatPanels(categg) {
     var newPanel = $("<div class='panel panel-default categ-panel'>")
     panelClass = "categ-" + categg
     newPanel.addClass(panelClass);
-
     var newPanelHTML = $("#panelHTML").html();
-    // $("#panels-view").empty();
     newPanel.html(newPanelHTML);
     $("#panels-view").append(newPanel);
     $("." + panelClass + " > >h3.panel-title").text(categg);
-    // $("."+panelClass+" >panel-body >").text(categg);
     $("." + panelClass + " >div.panel-body >").addClass(categg);
-
 }
 
-
-// -----------------------------------
-// SET CATEGORY DROPDOWN
 function setCateg() {
     var selectedCatBtn = $("#categ-dropdown-btn");
     var setCategTo = $(this).text() + " ";
-    console.log(this);
     selectedCatBtn.text(setCategTo);
     var spanCaret = $("<span> ").addClass("caret");
     selectedCatBtn.append(spanCaret);
 }
 $(".set-category-btn").click(setCateg);
 
-// -----------------------------------
 function showInCateg(category, newItem) {
 
     itemView = $("." + category);
@@ -75,32 +63,25 @@ function showInCateg(category, newItem) {
         var itemId = newItem.split(" ").join("-");
         var newItemRow = $(`<div id=${itemId} class='row item-row' >`);
         var inputRow = $("<input class='col-xs-10 row-eq-height item-row-input'>");
-        // var trashIcon = $(`<span class='glyphicon glyphicon-trash removeItem col-xs-1' data-category=${category} data-item=${newItem} ></span>`);
         inputRow.val(newItem);
         var checkBox = '<input type="checkbox" class="item-checkbx col-xs-1 row-eq-height" aria-label="..."> '
-        // newItemRow.append(trashIcon);
-
         newItemRow.append(`<button class= "col-xs-1 row-eq-height removeItem" data-category=${category} data-item=${itemId}>`);
         newItemRow.children("button").append("<span class='glyphicon glyphicon-trash'> ");  
         newItemRow.append(checkBox);
         newItemRow.append(inputRow)
-
         itemView.append(newItemRow);
     }
 
 }
-// ADD ITEM TO PACKLIST
 function addItem(event) {
     event.preventDefault();
     var SetTrip = JSON.parse(localStorage.getItem("SetTrip"));
     var cats = Object.keys(SetTrip.packingList);
-    // console.log(cats);
     var selectedCat = $("#categ-dropdown-btn").text().trim()
     var newItem = $("#item-input").val().trim();
     for (var i = 0; i < cats.length; i++) {
         var cat = cats[i];
         var categArray = SetTrip.packingList[cat]
-        // var newItem = $("#item-input").val().trim();
         var items = SetTrip.packingList[cats[i]];
         if (cat == selectedCat) {
             if (newItem) {
@@ -122,20 +103,14 @@ function addItem(event) {
     }
     localStorage.setItem("Trips Array", JSON.stringify(tripArray));
     $("#item-input").val("");
-
-
 }
 $("#add-item").click(addItem);
 
 $(document).on("click", ".removeItem", function () {
     var category = $(this).attr("data-category");
-    console.log(this);
-    console.log(category);
     var item = $(this).attr("data-item");
     var itemToRemove = item.split("-").join(" ");
-    console.log(item);
     var trip = JSON.parse(localStorage.getItem("SetTrip"));
-    console.log(trip, "this is the thing");
     var categoryArray = trip.packingList[category];
     var updatedArray = categoryArray.filter(catItem => catItem != itemToRemove);
     trip.packingList[category] = updatedArray;   
@@ -152,41 +127,30 @@ $(document).on("click", ".removeItem", function () {
     if (updatedArray.length === 0) {
         hideCatPanel(category);
     }
-
 });
 
 
-// function clickCustom(event) {
-//     event.preventDefault();
-//     console.log("add custom");
-// }
-// $(".custom-categ-btn").click(clickCustom);
-
+/** 
+ * 
+*/
 function addCateg() {
-
-
     event.preventDefault();
     newCateg = $("#custom-categ-input").val();
     if (newCateg != "") {
-        console.log(newCateg);
         var SetTrip = JSON.parse(localStorage.getItem("SetTrip"));
         SetTrip.packingList[newCateg] = [];
-        console.log(SetTrip)
         newListItem = "<li>"
         newLink = "<a href='#'>"
         newLink = $(newLink).text(newCateg);
         linkClass = "set-category-btn"
         newLink = $(newLink).addClass(linkClass);
         newListItem = $(newListItem).append(newLink);
-        console.log(newListItem)
         $(newListItem).insertBefore("li.divider");
-
         $("#myModal").modal("hide");    
         var selectedCatBtn = $("#categ-dropdown-btn");
         selectedCatBtn.text(newCateg+ " ");
         var spanCaret = $("<span> ").addClass("caret");
         selectedCatBtn.append(spanCaret)
-
         localStorage.setItem("SetTrip", JSON.stringify(SetTrip));
         var tripArray = JSON.parse(localStorage.getItem("Trips Array"));
         for(var i = 0; i <tripArray.length; i++){
@@ -200,6 +164,10 @@ function addCateg() {
 $("#add-categ-btn").click(addCateg);
 $(".set-category-btn").click(setCateg);
 
+/**
+ * Renders the list of categories from local storage as options in dropdown list
+ * @param {string} Categ 
+ */
 function renderDropList(Categ){
     var SetTrip = JSON.parse(localStorage.getItem("SetTrip"));
     for (var i = 0; i < SetTrip.packingList.length; i++){
@@ -208,7 +176,19 @@ function renderDropList(Categ){
 
 }
 
+/**
+ * Hides the category panel for a given category
+ * @param {string} categg 
+ */
 function hideCatPanel(categg){
     console.log(categg)
     $(".categ-" + categg).hide();
 }
+
+
+// function clickCustom(event) {
+//     event.preventDefault();
+//     console.log("add custom");
+// }
+// $(".custom-categ-btn").click(clickCustom);
+
